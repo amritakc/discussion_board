509276ea4bfbdf26383d3ac523b9caffdd0a551a
amritakc <amritakc@gmail.com>
2016-04-08 20:27:47 -0700

    added comment count to user table

--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -8,22 +8,24 @@ module.exports = (function() {
  return {
    create: function(req, res){
    		console.log(req.body)
-   		Answer.findOne({_id:req.body.userid}, function(err,answer){
-		  	var new_comment = new Comment({comment:req.body.text, user_name:req.body.user_name})
-		  	new_comment._answer = answer._id;
-		  	answer.comments.push(new_comment)
-		  	// console.log("about save" + new_answer)
-		  	new_comment.save(function(err,results){
-		  	answer.save(function(err){ 
-		  		if(err){
-		  			console.log("something went wrong")
-		  		}
-		  		else{
-		  			console.log("successfully added a comment!")
-		  			// console.log(results)
-		  			res.json(results)
-		  			}
+   		User.update({user_name: req.body.user_name}, {$inc: {total_comments:1}}, function(err, results){
+	   		Answer.findOne({_id:req.body.userid}, function(err,answer){
+			  	var new_comment = new Comment({comment:req.body.text, user_name:req.body.user_name})
+			  	new_comment._answer = answer._id;
+			  	answer.comments.push(new_comment)
+			  	// console.log("about save" + new_answer)
+			  	new_comment.save(function(err,results){
+			  	answer.save(function(err){ 
+			  		if(err){
+			  			console.log("something went wrong")
+			  		}
+			  		else{
+			  			console.log("successfully added a comment!")
+			  			// console.log(results)
+			  			res.json(results)
+			  			}
 	  	  		})
+		  	  })
 		 	})
 	  	})
   	  },
